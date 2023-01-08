6dfd011ae1e01824b3330dcbdf2dd1af532456a6
CagyJ <josh.jiao.322@gmail.com>
2023-01-08 18:22:01 +0100

    add body component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import './App.css'
+import Body from './components/Body';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="italic text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Header />
+      <Body />
     </div>
   );
 }
