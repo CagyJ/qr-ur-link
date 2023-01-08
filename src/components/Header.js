0acf25b617cb8116b927490b2295d9e0a0235e2c
CagyJ <josh.jiao.322@gmail.com>
2023-01-08 18:10:29 +0100

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header  = () => {
+
+    return (
+        <header className="bg-blue-800 p-4 mb-10">
+            <div className="max-w-5xl m-auto">
+                <div className="text-xl font-bold text-white">
+                    QR Your Link
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
